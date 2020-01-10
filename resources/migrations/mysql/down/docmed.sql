b80e8556e1476ba4f00bd94d9bfa3d42a2853c96
Peter Aba <peter@peteraba.com>
2020-01-10 22:40:42 +0100

    Add index page update to migrations

---
+++ resources/migrations/mysql/down/docmed.sql
@@ -0,0 +1,1 @@
+UPDATE pages SET title = 'New AbterCMS installation', layout_id = NULL, layout = '<div class="container">{{var/body}}</div>', deleted_at = NULL WHERE identifier = 'index';
---
+++ resources/migrations/mysql/up/docmed.sql
@@ -0,0 +1,6 @@
+UPDATE pages
+SET title      = 'New AbterCMS installation',
+    layout_id  = '3fc1212f-2189-4655-a11b-1a98dd897053',
+    layout     = '',
+    deleted_at = NULL
+WHERE identifier = 'index';
